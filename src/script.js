cca66d03ebd0c5a5fa0e7ef8196d573e723403e6
Giovanni Squillace <giovanni.squillace.work@gmail.com>
2024-03-01 14:05:26 -0500

    added api keys, updated git-ignore, and added a getNewAuthToken function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// If the user clicks to another tab, change the title of the page
+/* 
 document.addEventListener("visibilitychange", () => {
 	if (document.hidden) {
 		document.title = "Hey!! Come back ;(";
@@ -8,3 +10,67 @@ document.addEventListener("visibilitychange", () => {
 		}, 2000);
 	}
 });
+ */
+
+// API data
+
+/*
+* This Code is commented out for cost reasons, if you wish to add your own api data, simply add the correct URL, then go into api_keys.js to add you tokens
+
+
+import { publicToken, secretToken } from "../private/api_keys.js";
+const authentication_token_URL = "https://api.example.com/auth";
+
+async function getNewAuthToken(publicToken, secretToken) {
+	try {
+		const response = await fetch(authentication_token_URL, {
+			method: "POST",
+			headers: {
+				Accept: "text/plain",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				api_token: publicToken,
+				api_secret: secretToken,
+			}),
+		});
+
+		const data = await response.text();
+		console.log(data);
+		// Add whatever else you need here
+	} catch (error) {
+		console.error("Error:", error);
+	}
+}
+getNewAuthToken(publicToken, secretToken);
+*/
+
+//
+//
+//
+//
+//
+
+// If your using external API data, delete this section
+import fs from "fs";
+
+const filePath = "../carData.json";
+
+fs.readFile(filePath, "utf8", (err, data) => {
+	if (err) {
+		console.error("Error reading file:", err);
+		return;
+	}
+
+	const carData = JSON.parse(data);
+	console.log(carData);
+
+	/*
+	console.log(carData.makes[1]);
+	console.log(carData.makes[1].models[0]);
+	console.log(carData.makes[1].models[0].specs);
+	console.log(carData.makes[1].models[0].specs.engine);
+	console.log(carData.makes[1].models[0].specs.engine.max_hp);
+	*/
+});
+// End of file import
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { JWT_Url } from "./src/API-Token-Data.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+});
